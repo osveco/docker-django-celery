00ab1dbee5e1c6299d923a7c01069a619a77e00c
Zhe Brah <zhebrah@gmail.com>
2016-10-03 12:06:46 +0200

    [CONTP-94]: update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -6,7 +6,7 @@ from kombu import Exchange, Queue
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'megg_yej86ln@xao^+)it4e&ueu#!4tl9p1h%2sjr7ey0)m25f'
+SECRET_KEY = 'megg_yej86ln@xao^+)it4e&amp;ueu#!4tl9p1h%2sjr7ey0)m25f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -16,10 +16,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.staticfiles',
-
     'rest_framework',
     'myproject',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+
+    # required by Django 1.9
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -34,6 +39,8 @@ ROOT_URLCONF = 'myproject.urls'
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
+# Localization ant timezone settings
+
 TIME_ZONE = 'UTC'
 USE_TZ = True
 
@@ -47,12 +54,14 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
+
+# Database Condocker-composeuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_ENV_DB', 'postgres'),
         'USER': os.environ.get('DB_ENV_POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', ''),
+        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR', ''),
         'PORT': os.environ.get('DB_PORT_5432_TCP_PORT', ''),
     },
@@ -86,24 +95,35 @@ if not BROKER_URL.endswith(BROKER_HEARTBEAT):
 BROKER_POOL_LIMIT = 1
 BROKER_CONNECTION_TIMEOUT = 10
 
+# Celery configuration
 
+# configure queues, currently we have only one
 CELERY_DEFAULT_QUEUE = 'default'
 CELERY_QUEUES = (
     Queue('default', Exchange('default'), routing_key='default'),
 )
 
+# Sensible settings for celery
 CELERY_ALWAYS_EAGER = False
 CELERY_ACKS_LATE = True
 CELERY_TASK_PUBLISH_RETRY = True
 CELERY_DISABLE_RATE_LIMITS = False
+
+# By default we will ignore result
+# If you want to see results and try out tasks interactively, change it to False
+# Or change this setting on tasks level
 CELERY_IGNORE_RESULT = True
 CELERY_SEND_TASK_ERROR_EMAILS = False
+CELERY_TASK_RESULT_EXPIRES = 600
+
+# Set redis as celery result backend
 CELERY_RESULT_BACKEND = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
 CELERY_REDIS_MAX_CONNECTIONS = 1
-CELERY_TASK_RESULT_EXPIRES = 600
+
+# Don't use pickle as serializer, json is much safer
 CELERY_TASK_SERIALIZER = "json"
+CELERY_ACCEPT_CONTENT = ['application/json']
 
 CELERYD_HIJACK_ROOT_LOGGER = False
 CELERYD_PREFETCH_MULTIPLIER = 1
 CELERYD_MAX_TASKS_PER_CHILD = 1000
-CELERY_ACCEPT_CONTENT = ['application/json']
--- run_celery.sh
+++ run_celery.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# wait for broker container
+sleep 10
+
 cd myproject
+# run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 su -m myuser -c "celery worker -A myproject.celeryconf -Q default -n default@%h"
